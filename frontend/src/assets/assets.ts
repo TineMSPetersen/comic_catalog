1c7a274ffcdff5ca34ae1c970ee3378cf77b1dcb
Tine MS Petersen <tinemeretesikjaerpetersen@gmail.com>
2026-02-14 13:28:02 +0100

    Able to set status

--- frontend/src/assets/assets.ts
+++ frontend/src/assets/assets.ts
@@ -3,8 +3,10 @@ import star from './Star.svg'
 import openBook from './Open_Book.svg'
 import starOutline from './Star_Outline.svg'
 import starFilled from './Star_Filled.svg'
+import clock from './Clock.svg'
+import pause from './Pause.svg'
 
 export const assets = {
-  book, openBook,
+  book, openBook, clock, pause,
   star, starOutline, starFilled
 }
